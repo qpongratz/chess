c680ebb4c09b067b7d2ea0056cfec0579ced9d1a
qpongratz <lagarathan@gmail.com>
2021-01-26 11:31:57 -0600

    Create initialize method for piece

---
+++ lib/pieces/piece.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Controls the things all pieces need to be able to do.
+class Piece
+  def initialize(color, position)
+    @color = color
+    @position = position
+  end
+end
