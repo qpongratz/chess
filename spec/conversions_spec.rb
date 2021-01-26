2196ff556f58b917a03192877bdefa5c9ce0d02c
qpongratz <lagarathan@gmail.com>
2021-01-26 12:49:59 -0600

    Add tests for all methods of Conversions

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -15,5 +15,32 @@ describe Conversions do
       coordinates = convert.index_to_coordinates(index)
       expect(coordinates).to eq([7, 7])
     end
+    it 'Returns [0, 0]' do
+      index = 0
+      coordinates = convert.index_to_coordinates(index)
+      expect(coordinates).to eq([0, 0])
+    end
+    it 'Returns [4, 6]' do
+      index = 52
+      coordinates = convert.index_to_coordinates(index)
+      expect(coordinates).to eq([4, 6])
+    end
+  end
+  context 'From coordinates to index' do
+    it 'Returns 63' do
+      coordinates = [7, 7]
+      index = convert.coordinates_to_index(coordinates)
+      expect(index).to eq(63)
+    end
+    it 'Returns 0' do
+      coordinates = [0, 0]
+      index = convert.coordinates_to_index(coordinates)
+      expect(index).to eq(0)
+    end
+    it 'Returns 52' do
+      coordinates = [4, 6]
+      index = convert.coordinates_to_index(coordinates)
+      expect(index).to eq(52)
+    end
   end
 end
