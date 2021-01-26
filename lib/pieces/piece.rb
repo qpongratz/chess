58326960d831a4df1a123ddfdeb99db6d2ce0070
qpongratz <lagarathan@gmail.com>
2021-01-26 13:55:08 -0600

    Generalize arguments for piece initialize

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -2,8 +2,8 @@
 
 # Controls the things all pieces need to be able to do.
 class Piece
-  def initialize(color, position)
-    @color = color
-    @position = position
+  def initialize(args)
+    @color = args[:color]
+    @position = args[:position]
   end
 end
