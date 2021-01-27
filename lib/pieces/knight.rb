292e772a1e6d8186458c7e2ce000f1e6f02aecec
qpongratz <lagarathan@gmail.com>
2021-01-27 15:22:42 -0600

    Add knight class

---
+++ lib/pieces/knight.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+
+# Controls rook specific bits.
+class Knight < Piece
+
+  def post_initialize
+    @range = 1
+  end
+
+  def transformations
+    [[1, 2], [-2, -1], [-1, 2], [2, -1], [1, -2], [-2, 1], [-1, -2], [2, 1]]
+  end
+end
+
+my_knight = Knight.new('white', 0)
+p my_knight.sight
