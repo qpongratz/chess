4669627ce5638cdc0f6622580cd1fa80317473b7
qpongratz <lagarathan@gmail.com>
2021-02-01 10:37:20 -0600

    Define pawn transformations

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -7,17 +7,25 @@ class Pawn < Piece
   attr_reader :forward, :moved
 
   def post_initialize
+    @range = 1
     @forward = (color == 'white' ? -1 : 1)
     @moved = false
   end
 
   def position=(position)
     @position = index_to_coordinates(position)
-    update_sight
     @moved = true
+    update_sight
   end
 
   def transformations
-    
+    moves = [[0, forward], [-1, forward], [1, forward]]
+    moves.push([0, forward * 2]) unless moved
+    moves
   end
 end
+
+my_pawn = Pawn.new('white', 50)
+p my_pawn.sight
+p my_pawn.position = (41)
+p my_pawn.sight
