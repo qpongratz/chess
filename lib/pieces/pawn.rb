dc1c2898414b903731fd86831b2b7d12d3a06bcf
qpongratz <lagarathan@gmail.com>
2021-02-01 10:13:27 -0600

    Update Position setter for Pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -4,10 +4,17 @@ require_relative 'piece'
 
 # Controls rook specific bits.
 class Pawn < Piece
-  attr_reader :forward
+  attr_reader :forward, :moved
 
   def post_initialize
     @forward = (color == 'white' ? -1 : 1)
+    @moved = false
+  end
+
+  def position=(position)
+    @position = index_to_coordinates(position)
+    update_sight
+    @moved = true
   end
 
   def transformations
