0507ec9f2e0afce0ce2904969aa0dcb6203794ef
qpongratz <lagarathan@gmail.com>
2021-02-03 11:36:42 -0600

    Add setup for kings in king

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,9 +4,16 @@ require_relative 'piece'
 
 # Controls rook specific bits.
 class King < Piece
+  attr_reader :queen_rook, :king_rook, :board
 
   def post_initialize
     @range = 1
+  end
+
+  def setup(queen_rook, king_rook, board)
+    @queen_rook = queen_rook
+    @king_rook = king_rook
+    @board = board
   end
 
   def transformations
