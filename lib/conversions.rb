fc52b89db57d1a488b556f3f2e8748d74a63a643
qpongratz <lagarathan@gmail.com>
2021-02-04 10:23:25 -0600

    Add conversion for player_input_to_index

--- lib/conversions.rb
+++ lib/conversions.rb
@@ -13,4 +13,16 @@ module Conversions
     y = coordinates[1]
     x + (y * 8)
   end
+
+  def player_input_to_index(player_input)
+    input_coord = player_input.split('')
+    input_x = input_coord[0]
+    input_y = input_coord[1]
+    output_x = PLAYER_COLUMNS.index(input_x.upcase)
+    output_y = PLAYER_ROWS.index(input_y)
+    coordinates_to_index([output_x, output_y])
+  end
+
+  PLAYER_COLUMNS = %w[A B C D E F G H].freeze
+  PLAYER_ROWS = %w[8 7 6 5 4 3 2 1].freeze
 end
