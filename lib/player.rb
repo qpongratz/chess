8d528d016caafb14f5e56d1bf75fd8a3623a20f3
qpongratz <lagarathan@gmail.com>
2021-02-04 10:05:21 -0600

    Add method to get input in player

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require_relative 'conversions'
+
 # Handles player inputs
 class Player
+  include Conversions
   attr_reader :color
 
   def initialize(color)
@@ -9,5 +12,21 @@ class Player
   end
 
   def input_move(board)
+    p coordinates = input
+    # Get player input
+    # Separate to start and destination variables
+    # If it's valid, pass it to valid_move?(start, destination, color)
+    # If that's valid, pass it to move(start, destination)
+    # If not any of that, start over.
   end
+
+  def input(input = '')
+    puts 'Input your start and end positions space separated Example: "A4 B2"'
+    input = gets.chomp until input.match(/[a-h][1-8] [a-h][1-8]/i)
+    input
+  end
+
 end
+
+my_player = Player.new('white')
+my_player.input_move([nil])
