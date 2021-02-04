c681d6a0d53de425f17d3929142a56ef2a736bcc
qpongratz <lagarathan@gmail.com>
2021-02-03 19:04:47 -0600

    Refactor turn method

--- lib/game.rb
+++ lib/game.rb
@@ -1,28 +1,39 @@
 # frozen_string_literal: true
 
-require 'board'
-require 'player'
+require_relative 'board'
+require_relative 'player'
 
 # Controls the flow of the game.
 class Game
+  attr_reader :players, :board
   def initialize
-    board = Board.new
-    players = [Player.new('white'), Player.new('black')]
+    @board = Board.new
+    @players = [Player.new('white'), Player.new('black')]
   end
 
   def turn
-    current_player = players[0]
-    until board.no_moves?(current_player.color) == true
-      put "#{current_player.color}'s turn"
-      put 'You are in check' if board.check?(current_player.color)
+    until board.no_moves?(current_color) == true
+      turn_text
       current_player.input_move(board)
       players.rotate
-      current_player = players[0]
     end
     board.check?(current_player.color) ? checkmate : stalemate
   end
 
   private
+
+  def current_color
+    current_player.color
+  end
+
+  def current_player
+    players[0]
+  end
+
+  def turn_text
+    put "#{current_color}'s turn"
+    put 'You are in check' if board.check?(current_color)
+  end
 
   def checkmate
     'Checkmate. You lost and you won.'
