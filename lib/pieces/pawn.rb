7b222b12615d5d13a9fe4900f1f8b551c93d870b
qpongratz <lagarathan@gmail.com>
2021-02-05 09:03:47 -0600

    Refactor see? method

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'piece'
+require 'pry'
 
 # Controls rook specific bits.
 class Pawn < Piece
@@ -20,15 +21,14 @@ class Pawn < Piece
     update_sight
   end
 
-  def see?(args, actual_sight = [])
-    return false unless sight.flatten(1).include?(args[:destination])
-
+  def see?(args)
+    destination = args[:destination]
     board = args[:board]
-    actual_sight.push(sight[0]) if board.state[sight[0][0]].nil?
-    actual_sight.push(sight[1]) if board.colors_match?(sight[1][0], enemy_color) || board.en_passant_position == sight[1][0]
-    actual_sight.push(sight[2]) if board.colors_match?(sight[2][0], enemy_color) || board.en_passant_position == sight[2][0]
-    actual_sight.push(sight[3]) if !sight[3].nil? && board.state[sight[3][1]].nil?
-    actual_sight.flatten(1).include?(args[:destination])
+    possible_verticals = vertical_destinations.select { |spot| board.state[spot].nil? }
+    possible_diagonals = diagonal_destinations.select do |spot|
+      board.en_passant_position == spot || board.colors_match?(spot, enemy_color)
+    end
+    (possible_diagonals + possible_verticals).flatten.include?(destination)
   end
 
   def to_s
@@ -56,6 +56,17 @@ class Pawn < Piece
 
   def correct_sight
     sight[3] && sight[3] = sight[0] + sight[3]
+  end
+
+  def vertical_destinations
+    destinations = [sight.flatten.first]
+    destinations.push(sight.flatten.last) if sight.length == 4
+    destinations
+  end
+
+  def diagonal_destinations
+    destinations = [sight[1], sight[2]]
+    destinations.flatten
   end
 
   def transformations
