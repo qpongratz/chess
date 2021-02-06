c66165814ad823c600cef99667bce6c7c9fc6e70
qpongratz <lagarathan@gmail.com>
2021-02-06 14:39:11 -0600

    Add ability to choose promotion and save game

--- lib/player.rb
+++ lib/player.rb
@@ -14,20 +14,40 @@ class Player
   def input_move(board)
     loop do
       coordinates = input
+      return 'save' if coordinates.downcase == 'save'
+
       start = player_input_to_index(coordinates[0])
       destination = player_input_to_index(coordinates[1])
       # Alternate loop break for saving
-      if board.valid_move?(start, destination, color)
-        board.move(start, destination)
-        break
-      end
+      next unless board.valid_move?(start, destination, color)
+
+      board.move(start, destination)
+      board.promote(choose_promtion, color) unless board.promotion.nil?
+      break
     end
+  end
+
+  private
+
+  def choose_promotion(input = '')
+    display_promotion_options
+    input = gets.chomp until input_valid_promotion?
+    input.to_i
   end
 
   def input(input = '')
     puts 'Input your start and end positions space separated Example: "A4 B2"'
-    input = gets.chomp until input.match(/[a-h][1-8] [a-h][1-8]/i)
-    # Have to change check when you can save
+    input = gets.chomp until input_valid_move?(input)
     input.split(' ')
+  end
+
+  def input_valid_move?(input)
+    input.match(/[a-h][1-8] [a-h][1-8]/i) ||
+      input.downcase == 'save'
+  end
+
+  def input_valid_promotion?(input)
+    input.match(/[1-4]/) ||
+      input.downcase == 'save'
   end
 end
