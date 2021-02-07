43607705c762d6c2f6458ae70d503ffff75e565d
qpongratz <lagarathan@gmail.com>
2021-02-07 10:58:23 -0600

    Fix logic and typos for promotion

--- lib/player.rb
+++ lib/player.rb
@@ -14,15 +14,15 @@ class Player
   def input_move(board)
     loop do
       coordinates = input
-      return 'save' if coordinates.downcase == 'save'
+      p coordinates
+      return 'save' if coordinates[0] == 'save'
 
       start = player_input_to_index(coordinates[0])
       destination = player_input_to_index(coordinates[1])
-      # Alternate loop break for saving
       next unless board.valid_move?(start, destination, color)
 
       board.move(start, destination)
-      board.promote(choose_promtion, color) unless board.promotion.nil?
+      board.promote(choose_promotion, color) unless board.promotion.nil?
       break
     end
   end
@@ -30,24 +30,24 @@ class Player
   private
 
   def choose_promotion(input = '')
-    display_promotion_options
-    input = gets.chomp until input_valid_promotion?
+    #display_promotion_options
+    puts "1, queen. 2, knight, 3, rook, 4, bishop"
+    input = gets.chomp until input_valid_promotion?(input)
     input.to_i
   end
 
   def input(input = '')
     puts 'Input your start and end positions space separated Example: "A4 B2"'
-    input = gets.chomp until input_valid_move?(input)
+    input = gets.chomp.downcase until input_valid_move?(input)
     input.split(' ')
   end
 
   def input_valid_move?(input)
     input.match(/[a-h][1-8] [a-h][1-8]/i) ||
-      input.downcase == 'save'
+      input == 'save'
   end
 
   def input_valid_promotion?(input)
-    input.match(/[1-4]/) ||
-      input.downcase == 'save'
+    input.match(/[1-4]/)
   end
 end
